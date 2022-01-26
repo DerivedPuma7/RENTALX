a397dde286e2755bfc648df7442ca4e68f5073f0
Gustavo Figueiredo <gustavofigueiredo@deltaglobal.com.br>
2022-01-26 07:37:23 -0300

    refatorando as rotas

--- src/server.ts
+++ src/server.ts
@@ -1,14 +1,8 @@
 import express from "express";
-import categoriesRoutes from './routes/categories.routes';
-import specificationsRoutes from "./routes/specification.routes";
+import router from './routes';
 
 const app = express();
 app.use(express.json());
-app.use('/categories', categoriesRoutes);
-app.use('/specification', specificationsRoutes);
-
-app.get('/', (request, response) => {
-   return response.json({ status: "dale dele dele doly" });
-});
+app.use(router);
 
 app.listen(3333, () => console.log("rodandoooo"));
