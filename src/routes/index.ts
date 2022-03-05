f2bb6cb6c17bcb618975a56f2000914bf69fd22e
Gustavo Figueiredo <gustavoribeiro665@hotmail.com>
2022-03-05 19:56:04 -0300

    trabalhando com autenticacao jwt

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -3,11 +3,13 @@ import { Router } from 'express';
 import categoriesRoutes from './categories.routes';
 import specificationRoutes from './specification.routes';
 import usersRoutes from './users.routes';
+import authenticateRoutes from './authenticate.routes';
 
 const router = Router();
 
 router.use('/categories', categoriesRoutes);
 router.use('/specification', specificationRoutes);
 router.use('/users', usersRoutes);
+router.use(authenticateRoutes)
 
 export default router;
