51a2167767fe049e8d99d29037df85d66dacb6a7
Gustavo Figueiredo <gustavoribeiro665@hotmail.com>
2022-03-06 19:00:38 -0300

    deletando arquivo de avatar do usuario antes de inserir um novo

--- src/modules/account/useCases/updateUserAvatar/updateUserAvatarUseCase.ts
+++ src/modules/account/useCases/updateUserAvatar/updateUserAvatarUseCase.ts
@@ -1,5 +1,6 @@
 import { inject, injectable } from "tsyringe";
 import IUsersRepository from "../../repositories/IUsersRepository";
+import { deleteFile } from "../../../../utils/file";
 
 interface IRequest{
     user_id: string;
@@ -13,6 +14,10 @@ class UpdateUserAvatarUseCase{
 
     async execute({user_id, avatar_file}: IRequest): Promise<void>{
         const user = await this.usersRepository.findById(user_id);
+
+        if(user.avatar){
+            await deleteFile(`./tmp/avatar/${user.avatar}`);
+        }
 
         user.avatar = avatar_file;
 
