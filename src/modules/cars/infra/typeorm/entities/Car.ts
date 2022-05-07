d91081872ca99b4b51106b564cf385e4baea43f6
Gustavo Figueiredo <gustavofigueiredo@deltaglobal.com.br>
2022-05-07 16:28:47 -0300

    ajustando o relacionamento na entidade Car

--- src/modules/cars/infra/typeorm/entities/Car.ts
+++ src/modules/cars/infra/typeorm/entities/Car.ts
@@ -1,6 +1,7 @@
-import { Column, CreateDateColumn, Entity, JoinColumn, ManyToOne, PrimaryColumn } from 'typeorm';
+import { Column, CreateDateColumn, Entity, JoinColumn, JoinTable, ManyToMany, ManyToOne, PrimaryColumn } from 'typeorm';
 import { v4 as uuidV4 } from 'uuid';
 import Category from './Category';
+import Specification from './Specification';
 @Entity("cars")
 class Car {
     @PrimaryColumn()
@@ -33,6 +34,14 @@ class Car {
 
     @Column()
     category_id: string;
+
+    @ManyToMany(() => Specification)
+    @JoinTable({
+        name: "specifications_cars",
+        joinColumns: [{name: "car_id"}], //nome da coluna na nossa tabela de relacionamento que referencia essa nossa tabela "Cars"
+        inverseJoinColumns: [{name: "especification_id"}] //nome da coluna na nossa tabela que referencia a nossa tabela que indicamos no ManyToMany
+    })
+    specifications: Specification[];
 
     @CreateDateColumn()
     created_at: Date;
