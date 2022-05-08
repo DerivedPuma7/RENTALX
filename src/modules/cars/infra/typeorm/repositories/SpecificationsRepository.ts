ea3e852b05d07538dace570dbe37e5c6e43148c5
Gustavo Figueiredo <gustavofigueiredo@deltaglobal.com.br>
2022-05-08 16:28:24 -0300

    ajustando o repositorio de especificacao

--- src/modules/cars/infra/typeorm/repositories/SpecificationsRepository.ts
+++ src/modules/cars/infra/typeorm/repositories/SpecificationsRepository.ts
@@ -6,24 +6,30 @@ import CategoriesRepository from './CategoriesRepository';
 class SpecificationsRepository implements ISpecificationsRepository {
    private repository: Repository<Specification>;
 
-   private static INSTANCE: SpecificationsRepository;
-
    constructor() {
       this.repository = getRepository(Specification);
    }
 
-   async create({ name, description }: ICreateSpecificationDTO): Promise<void> {
+   async create({ name, description }: ICreateSpecificationDTO): Promise<Specification> {
       const specification = this.repository.create({
          name,
          description
       });
 
       await this.repository.save(specification);
+
+      return specification;
    }
 
    async findByName(name: string): Promise<Specification> {
-      const specification = this.repository.findOne({name});
+      const specification = await this.repository.findOne({name});
       return specification;
+   }
+
+   async findByIds(ids: string[]): Promise<Specification[]> {
+      const specifications = await this.repository.findByIds(ids);
+
+      return specifications;
    }
 
 }
