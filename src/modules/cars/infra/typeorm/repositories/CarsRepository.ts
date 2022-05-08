d5ef3dc54ff02f430cb2c0f704f280fe8c7cb5c1
Gustavo Figueiredo <gustavofigueiredo@deltaglobal.com.br>
2022-05-08 16:29:34 -0300

    ajustando a interface e o repositorio de carros

--- src/modules/cars/infra/typeorm/repositories/CarsRepository.ts
+++ src/modules/cars/infra/typeorm/repositories/CarsRepository.ts
@@ -12,9 +12,9 @@ class CarsRepository implements ICarsRepository {
         this.repository = getRepository(Car);
     }
     
-    async create({ brand, category_id, daily_rate, description, fine_amount, license_plate, name }: ICreateCarDTO): Promise<Car> {
+    async create({ brand, category_id, daily_rate, description, fine_amount, license_plate, name, specifications, id }: ICreateCarDTO): Promise<Car> {
 
-        const car = this.repository.create({ brand, category_id, daily_rate, description, fine_amount, license_plate, name });
+        const car = this.repository.create({ brand, category_id, daily_rate, description, fine_amount, license_plate, name, specifications, id });
         await this.repository.save(car);
 
         return car;
@@ -47,8 +47,8 @@ class CarsRepository implements ICarsRepository {
         return cars;
     }
 
-    async findById(car_id: string): Promise<Car> {
-        const car = await this.repository.findOne({ id: car_id });
+    async findById(id: string): Promise<Car> {
+        const car = await this.repository.findOne(id);
 
         return car;
     }
--- src/modules/cars/repositories/ICarsRepository.ts
+++ src/modules/cars/repositories/ICarsRepository.ts
@@ -6,7 +6,7 @@ interface ICarsRepository {
     create(data: ICreateCarDTO): Promise<Car>;
     findByLicensePlate(license_plate: string): Promise<Car>;
     findAvailable(category_id?: string, brand?: string, name?: string): Promise<Car[]>;
-    findById(car_id: string): Promise<Car>;
+    findById(id: string): Promise<Car>;
 }
 
 export default ICarsRepository;
