20b1a676be056f243d7e450db340d4358f3d23dc
Gustavo Figueiredo <gustavofigueiredo@deltaglobal.com.br>
2022-05-08 19:01:52 -0300

    rota, controller e useCase para o upload de imagens dos carros

--- src/shared/infra/http/routes/cars.routes.ts
+++ src/shared/infra/http/routes/cars.routes.ts
@@ -1,18 +1,25 @@
 import { Router } from "express";
+import multer from "multer";
 import CreateCarController from "@modules/cars/useCases/createCar/CreateCarController";
 import { ensureAuthenticated } from "../middleweres/ensureAuthenticate";
 import { ensureAdmin } from "../middleweres/ensureAdmin";
 import ListAvailableCarsController from "@modules/cars/useCases/listAvailableCars/ListAvailableCarsController";
 import CreateCarSpecificationController from "@modules/cars/useCases/CreateCarSpecification/CreateCarSpecificationController";
+import UploadCarImageController from "@modules/cars/useCases/uploadCarImage/UploadCarImageController";
+import uploadConfig from '@config/upload';
 
 const carsRoutes = Router();
 const createCarController = new CreateCarController();
 const listAvailableCarsController = new ListAvailableCarsController();
 const createCarSpecificationController = new CreateCarSpecificationController();
+const uploadCarImageController = new UploadCarImageController();
+
+const upload = multer(uploadConfig.upload("./tmp/cars"));
 
 carsRoutes.post('/', ensureAuthenticated, ensureAdmin, createCarController.handle);
 carsRoutes.get('/available', listAvailableCarsController.handle);
 carsRoutes.post('/specifications/:car_id', ensureAuthenticated, ensureAdmin, createCarSpecificationController.handle);  
+carsRoutes.post('/images/:car_id', ensureAuthenticated, ensureAdmin, upload.array("images"), uploadCarImageController.handle);
 
 
 export default carsRoutes;
