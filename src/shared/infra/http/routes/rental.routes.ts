e47c941bdcc114797f2f074a8547f98b3bf6a24b
Gustavo Figueiredo <gustavofigueiredo@deltaglobal.com.br>
2022-05-22 20:25:11 -0300

    rota da devolução de aluguel

--- src/shared/infra/http/routes/rental.routes.ts
+++ src/shared/infra/http/routes/rental.routes.ts
@@ -1,10 +1,14 @@
 import CreateRentalController from "@modules/rentals/useCases/CreateRental/CreateRentalController";
+import DevolutionRentalController from "@modules/rentals/useCases/DevolutionRental/DevolutionRentalController";
 import { Router } from "express";
 import { ensureAuthenticated } from "../middleweres/ensureAuthenticate";
 
 const rentalRoutes = Router();
 const createRentalController = new CreateRentalController();
+const devolutionRentalController = new DevolutionRentalController();
 
-rentalRoutes.post("/", ensureAuthenticated, createRentalController.handle)
+rentalRoutes.post("/", ensureAuthenticated, createRentalController.handle);
+rentalRoutes.post("/devolution/:id", ensureAuthenticated, devolutionRentalController.handle);
+
 
 export { rentalRoutes };
