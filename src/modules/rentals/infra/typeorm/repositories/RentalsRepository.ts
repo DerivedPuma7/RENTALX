f786b5a416aaa9f31db0b53007dae13bfd9d8cae
Gustavo Figueiredo <gustavofigueiredo@deltaglobal.com.br>
2022-05-22 21:13:36 -0300

    ajustando interface e implementando metodo no repositorio

--- src/modules/rentals/infra/typeorm/repositories/RentalsRepository.ts
+++ src/modules/rentals/infra/typeorm/repositories/RentalsRepository.ts
@@ -37,6 +37,13 @@ class RentalsRepository implements IRentalsRepository {
     async findById(id: string): Promise<Rental> {
         return await this.repository.findOne({ id });
     }
+
+    async findRentalByUser(user_id: string): Promise<Rental[]> {
+        return this.repository.find({
+            where: {user_id},
+            relations: ["car"]
+        });
+    }
 }
 
 export default RentalsRepository;
--- src/modules/rentals/repositories/IRentalsRepository.ts
+++ src/modules/rentals/repositories/IRentalsRepository.ts
@@ -7,6 +7,7 @@ interface IRentalsRepository {
     findOpenRentalByCar(car_id: string): Promise<Rental>;
     findOpenRentalByUser(user_id: string): Promise<Rental>;
     findById(id: string): Promise<Rental>;
+    findRentalByUser(user_id: string): Promise<Rental[]>;
 }
 
 export default IRentalsRepository;
