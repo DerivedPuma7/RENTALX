e17c39e3f46fbe0648c09caba67493a97f7207d6
Gustavo Figueiredo <gustavofigueiredo@deltaglobal.com.br>
2022-05-22 21:12:51 -0300

    criando a listagem de alugueis para o usuario logado

--- src/shared/infra/http/routes/rental.routes.ts
+++ src/shared/infra/http/routes/rental.routes.ts
@@ -1,13 +1,16 @@
 import CreateRentalController from "@modules/rentals/useCases/CreateRental/CreateRentalController";
 import DevolutionRentalController from "@modules/rentals/useCases/DevolutionRental/DevolutionRentalController";
+import ListRentalsByUserController from "@modules/rentals/useCases/ListRentalsByUser/ListRentalsByUserController";
 import { Router } from "express";
 import { ensureAuthenticated } from "../middleweres/ensureAuthenticate";
 
 const rentalRoutes = Router();
 const createRentalController = new CreateRentalController();
 const devolutionRentalController = new DevolutionRentalController();
+const listRentalsByUserController = new ListRentalsByUserController();
 
 rentalRoutes.post("/", ensureAuthenticated, createRentalController.handle);
+rentalRoutes.get("/user", ensureAuthenticated, listRentalsByUserController.handle);
 rentalRoutes.post("/devolution/:id", ensureAuthenticated, devolutionRentalController.handle);
 
 
