42375e26eae5a37dcc02e9d132f9785b44eae6e5
Gustavo Figueiredo <gustavofigueiredo@deltaglobal.com.br>
2022-05-24 20:37:40 -0300

    FEAT: implementado metodo que faltava no repositorio em memoria dos rentals

--- src/modules/rentals/repositories/in-memory/RentalsRepositoryInMemory.ts
+++ src/modules/rentals/repositories/in-memory/RentalsRepositoryInMemory.ts
@@ -32,6 +32,10 @@ class RentalsRepositoryInMemory implements IRentalsRepository {
     async findById(id: string): Promise<Rental> {
         return this.rentals.find((rental) => rental.id === id);
     }
+
+    async findRentalByUser(user_id: string): Promise<Rental[]> {
+        return this.rentals.filter((rental) => rental.user_id === user_id);
+    }
 }
 
 export default RentalsRepositoryInMemory;
