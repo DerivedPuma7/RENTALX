854093d402aa61919d6af6313f29c192b3bc4c65
Gustavo Figueiredo <gustavofigueiredo@deltaglobal.com.br>
2022-05-24 20:40:50 -0300

    test: ajustando o teste de criação de especificação do carro

--- src/modules/cars/useCases/CreateCarSpecification/CreateCarSpecificationUseCase.spec.ts
+++ src/modules/cars/useCases/CreateCarSpecification/CreateCarSpecificationUseCase.spec.ts
@@ -16,12 +16,12 @@ describe("Create Car Specification", () => {
     });
 
     it("should not be able to add a new specification to a non existing car", async () => {
-        expect(async () => {
-            const car_id = "1234";
-            const specifications_id = ["54321"];
+        const car_id = "1234";
+        const specifications_id = ["54321"];
 
-            await createCarSpecificationUseCase.execute({ car_id, specifications_id });
-        }).rejects.toBeInstanceOf(AppError);
+        expect(createCarSpecificationUseCase
+            .execute({ car_id, specifications_id })
+        ).rejects.toEqual(new AppError("Car does not exists!"));
     });
 
     it("should be able to add a new specification to the car", async () => {
