52ed52461acaca5fa13a7d24419e47e356426798
Gustavo Figueiredo <gustavofigueiredo@deltaglobal.com.br>
2022-05-25 22:56:28 -0300

    feat: constants com as infos de autenticação

---
+++ src/config/auth.ts
@@ -0,0 +1,7 @@
+export default {
+    secret_token: "3a3c094a19bc869da75e6815f0c07cc5",
+    expires_in_token: "15m",
+    secret_refresh_token: "d8253d2f31ae66753e4c6f307d768023",
+    expires_in_refresh_token: "30d",
+    expires_refresh_token_days: 30
+}
