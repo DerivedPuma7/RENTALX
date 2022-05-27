52eb48f2f5e2d04cb2791694f2a3ba276df4c19a
Gustavo Figueiredo <gustavofigueiredo@deltaglobal.com.br>
2022-05-26 21:22:39 -0300

    feat: controller e useCase do password recovery

---
+++ src/modules/account/useCases/sendForgotPasswordMail/SendForgotPasswordMailUseCase.ts
@@ -0,0 +1,43 @@
+import { v4 as uuidV4 } from "uuid";
+import IUsersRepository from "@modules/account/repositories/IUsersRepository";
+import IUsersTokensRepository from "@modules/account/repositories/IUsersTokensRepository";
+import AppError from "@shared/errors/AppError";
+import { inject, injectable } from "tsyringe";
+import IDateProvider from "@shared/container/providers/DateProvider/IDateProvider";
+import IMailProvider from "@shared/container/providers/MailProvider/IMailProvider";
+
+
+@injectable()
+class SendForgotPasswordMailUseCase {
+    constructor(
+        @inject("UsersRepository")
+        private usersRepository: IUsersRepository,
+        @inject("UsersTokensRepository")
+        private usersTokensRepository: IUsersTokensRepository,
+        @inject("DayJsDateProvider")
+        private dateProvider: IDateProvider,
+        @inject("EtherealMailProvider")
+        private mailProvider: IMailProvider
+    ) {}
+
+    async execute(email: string): Promise<void> {
+        const user = await this.usersRepository.findByEmail(email);
+
+        if(!user) {
+            throw new AppError("User does not exists!");
+        }
+
+        const token = uuidV4();
+        const tokenExpiresDate = this.dateProvider.addHours(3);
+        
+        await this.usersTokensRepository.create({
+            refresh_token: token,
+            user_id: user.id,
+            expires_date: tokenExpiresDate
+        });
+
+        await this.mailProvider.sendMail(email, "Recuperação de senha", `O link para o reset é ${token}`);
+    }
+}
+
+export default SendForgotPasswordMailUseCase;
