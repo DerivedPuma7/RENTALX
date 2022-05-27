5abe3df8adae543dfc6aad8769b04d9b89074c34
Gustavo Figueiredo <gustavofigueiredo@deltaglobal.com.br>
2022-05-26 21:20:45 -0300

    feat: registrando o novo container de injeção

--- src/shared/container/providers/index.ts
+++ src/shared/container/providers/index.ts
@@ -1,8 +1,19 @@
 import { container } from "tsyringe";
+
+//date provider
 import IDateProvider from "./DateProvider/IDateProvider";
 import DayJsDateProvider from "./DateProvider/implementations/DayJsDateProvider";
+
+//mail provider
+import IMailProvider from "./MailProvider/IMailProvider";
+import EtherealMailProvider from "./MailProvider/implementations/EtherealMailProvider";
 
 container.registerSingleton<IDateProvider>(
     "DayJsDateProvider",
     DayJsDateProvider
-)
+);
+
+container.registerInstance<IMailProvider>(
+    "EtherealMailProvider",
+    new EtherealMailProvider()
+);
