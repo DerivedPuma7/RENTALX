94dc82fef8b3e0925dea41abfe951b6afb370c2f
Gustavo Figueiredo <gustavofigueiredo@deltaglobal.com.br>
2022-05-27 22:41:11 -0300

    fix: corrigindo import

--- src/modules/account/useCases/refreshToken/RefreshTokenController.ts
+++ src/modules/account/useCases/refreshToken/RefreshTokenController.ts
@@ -1,6 +1,6 @@
 import { Request, Response } from "express";
 import { container } from "tsyringe";
-import RefreshTokenUseCase from "./refreshTokenUseCase";
+import RefreshTokenUseCase from "./RefreshTokenUseCase";
 
 
 class RefreshTokenController {
