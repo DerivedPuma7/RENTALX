f61bb0b8eb22cf80ee675c3602945ed446040c3d
Gustavo Figueiredo <gustavofigueiredo@deltaglobal.com.br>
2022-05-28 11:53:14 -0300

    fix: usando a constant no lugar da string do token

--- src/shared/infra/http/middleweres/ensureAuthenticate.ts
+++ src/shared/infra/http/middleweres/ensureAuthenticate.ts
@@ -2,6 +2,7 @@ import { NextFunction, Request, Response } from "express";
 import { verify } from "jsonwebtoken";
 import AppError from "@shared/errors/AppError";
 import UsersRepository from "@modules/account/infra/typeorm/repositories/UsersRepository";
+import auth from "@config/auth";
 
 interface IPayload{
     sub: string;
@@ -21,7 +22,7 @@ export async function ensureAuthenticated(request: Request, response: Response, 
     const [, token] = authHeader.split(" ");
 
     try {
-        const { sub: user_id } = verify(token, "3a3c094a19bc869da75e6815f0c07cc5") as IPayload;
+        const { sub: user_id } = verify(token, auth.secret_token) as IPayload;
 
         const usersRepository = new UsersRepository();
         const user = await usersRepository.findById(user_id);
