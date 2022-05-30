b79618b7f3a35b2c8bf936abd2befc329fdac3b1
Gustavo Figueiredo <gustavofigueiredo@deltaglobal.com.br>
2022-05-30 20:10:35 -0300

    refactor: alterando o useCase de atualizar o avatar do usuario. Vamos usar o provider

--- src/modules/account/useCases/updateUserAvatar/updateUserAvatarUseCase.ts
+++ src/modules/account/useCases/updateUserAvatar/updateUserAvatarUseCase.ts
@@ -1,6 +1,6 @@
 import { inject, injectable } from "tsyringe";
 import IUsersRepository from "@modules/account/repositories/IUsersRepository";
-import { deleteFile } from "@utils/file";
+import IStorageProvider from "@shared/container/providers/StorageProvider/IStorageProvider";
 
 interface IRequest{
     user_id: string;
@@ -10,15 +10,21 @@ interface IRequest{
 @injectable()
 class UpdateUserAvatarUseCase{
 
-    constructor(@inject("UsersRepository") private usersRepository: IUsersRepository) {}
+    constructor(
+        @inject("UsersRepository") 
+        private usersRepository: IUsersRepository,
+        @inject("StorageProvider")
+        private storageProvider: IStorageProvider
+    ) {}
 
     async execute({user_id, avatar_file}: IRequest): Promise<void>{
         const user = await this.usersRepository.findById(user_id);
 
         if(user.avatar){
-            await deleteFile(`./tmp/avatar/${user.avatar}`);
+            await this.storageProvider.delete(user.avatar, "avatar");
         }
 
+        await this.storageProvider.save(avatar_file, "avatar");
         user.avatar = avatar_file;
 
         await this.usersRepository.create(user);
