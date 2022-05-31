14b5ad71ea9244f7271d76c1418277a75650ea3b
Gustavo Figueiredo <gustavofigueiredo@deltaglobal.com.br>
2022-05-30 22:57:22 -0300

    feat: apontando o /avatar e o /cars pra nossa pasta de imagens temp

--- src/shared/infra/http/app.ts
+++ src/shared/infra/http/app.ts
@@ -7,12 +7,15 @@ import "@shared/container";
 import router from '@shared/infra/http/routes';
 import swaggerFile from '../../../swagger.json';
 import AppError from "@shared/errors/AppError";
+import upload from "@config/upload";
 
 createConnection();
 const app = express();
 
 app.use(express.json());
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerFile));
+app.use('/avatar', express.static(`${upload.tmpFolder}/avatar`));
+app.use('/cars', express.static(`${upload.tmpFolder}/cars`));
 app.use(router);
 
 app.use((err: Error, request: Request, response: Response, next: NextFunction) => {
