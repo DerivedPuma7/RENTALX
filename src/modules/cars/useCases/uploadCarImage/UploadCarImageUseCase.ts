53b7fac912ccc1d7ca3fb7b8a911e76abde5d30d
Gustavo Figueiredo <gustavofigueiredo@deltaglobal.com.br>
2022-05-30 22:56:05 -0300

    refactor: ajustando o useCase de salvar imagens do carro para usar nosso provider

--- src/modules/cars/useCases/uploadCarImage/UploadCarImageUseCase.ts
+++ src/modules/cars/useCases/uploadCarImage/UploadCarImageUseCase.ts
@@ -1,4 +1,5 @@
 import ICarsImageRepository from "@modules/cars/repositories/ICarsImageRepository";
+import IStorageProvider from "@shared/container/providers/StorageProvider/IStorageProvider";
 import { inject, injectable } from "tsyringe";
 
 interface IRequest {
@@ -10,13 +11,16 @@ interface IRequest {
 class UploadCarImageUseCase{
     constructor(
         @inject("CarsImageRepository")
-        private carsImageRepository: ICarsImageRepository
+        private carsImageRepository: ICarsImageRepository,
+        @inject("StorageProvider")
+        private storageProvider: IStorageProvider
     ){}
 
     async execute({ car_id, images_name }: IRequest): Promise<void> {
         images_name.map(async (image) => {
             await this.carsImageRepository.create(car_id, image);
-        })
+            await this.storageProvider.save(image, "cars");
+        });
     }
 }
 
