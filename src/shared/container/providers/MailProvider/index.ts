25bd18998dc1b36db67368d0f1eadd5ed196c2c2
Gustavo Figueiredo <gustavofigueiredo@deltaglobal.com.br>
2022-06-04 15:56:36 -0300

    fix: ajustando import

--- src/shared/container/providers/MailProvider/index.ts
+++ src/shared/container/providers/MailProvider/index.ts
@@ -3,7 +3,7 @@ import { container } from "tsyringe";
 //mail provider
 import IMailProvider from "./IMailProvider";
 import EtherealMailProvider from "./implementations/EtherealMailProvider";
-import SESMailProvider from "./implementations/SesMailProvider";
+import SESMailProvider from "./implementations/SESMailProvider";
 
 const mailProvider = {
     ethereal: container.resolve(EtherealMailProvider),
